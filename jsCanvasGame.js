b734131796f0614992c65527eb2c6689dd724961
Leah Bovat <lbov.ct@gmail.com>
2022-11-29 10:27:55 -0500

    added player bar and collision detection
    minor bug with enemy bar collision ( :( )

--- jsCanvasGame.js
+++ jsCanvasGame.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("demoCanvas");
 const start = document.getElementById("startBtn");
 const stop = document.getElementById("stopBtn");
 const ctx = canvas.getContext("2d");
+var keys = {};
 let raf;
 
 const ball = {
@@ -17,14 +18,34 @@ const ball = {
     ctx.closePath();
     ctx.fillStyle = this.color;
     ctx.fill();
+  },
+  move() {
+    if (ballCollision() == true) {
+      this.x = -this.x;
+      ball.draw();
+      playerBar.draw();
+      enemyBar.draw();
+    }
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x + this.vx > canvas.width || this.x + this.vx < 0) {
+      this.vx = -this.vx;
+    }
+
+    if (this.y + this.vy > canvas.height || this.y + this.vy < 0) {
+      this.vy = -this.vy;
+    }
   }
 }
 
-const bar = {
-  x: 100,
-  y: canvas.height / 2,
-  width: 25,
+const playerBar = {
+  width: 20,
   height: 100,
+  x: 50,
+  y: (canvas.height / 2) - 25,
+  vy: 5,
   color: "#235800",
   draw() {
     ctx.fillStyle = this.color;
@@ -32,8 +53,25 @@ const bar = {
   }
 }
 
+const enemyBar = {
+  width: playerBar.width,
+  height: playerBar.height,
+  x: canvas.width - 50,
+  y: (canvas.height / 2) - 75,
+  vy: 5,
+  color: playerBar.color,
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+//init
 if (canvas.getContext) {
   ball.draw();
+  playerBar.draw();
+  enemyBar.draw();
+
   start.addEventListener("click", (e) => {
     raf = window.requestAnimationFrame(draw);
   });
@@ -44,28 +82,51 @@ if (canvas.getContext) {
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
     ball.draw();
+    playerBar.draw();
+    enemyBar.draw();
   });
 
-  addEventListener() {
-    
+  window.addEventListener("keydown", (e) => {
+    keys[e.key] = true;
+    e.preventDefault();
+  });
+  window.addEventListener("keyup", (e) => {
+    delete keys[e.key];
+  });
+}
+
+function input() {
+  if ("w" in keys) {
+    if (playerBar.y > 25) {
+      playerBar.y -= playerBar.vy;
+    }
+  } else if ("s" in keys) {
+    if (playerBar.y < canvas.height - (playerBar.height + 25)) {
+      playerBar.y += playerBar.vy;
+    }
+  }
+}
+
+function enemyMove() {
+
+}
+
+function ballCollision() {
+  if (((ball.x + ball.vx <= playerBar.x + playerBar.width) && (ball.y + ball.vy > playerBar.y) && (ball.y + ball.vy <= playerBar.y + playerBar.height)) || ((ball.x + ball.vx >= enemyBar.x) && (ball.y + ball.vy > enemyBar.y) && (ball.y + ball.vy <= enemyBar.y + enemyBar.height))) {
+    return true;
+  } else {
+    return false;
   }
 }
 
 function draw() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   ball.draw();
-  bar.draw();
+  playerBar.draw();
+  enemyBar.draw();
+  input();
 
-  ball.x += ball.vx;
-  ball.y += ball.vy;
-
-  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
-    ball.vx = -ball.vx;
-  }
-
-  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-    ball.vy = -ball.vy;
-  }
+  ball.move();
 
   raf = window.requestAnimationFrame(draw);
 }
